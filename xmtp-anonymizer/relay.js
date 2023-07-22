5fc606decf5d35fd5c3d22c1aba4546e9c2060a9
Jason Sun <sunapi386@gmail.com>
2023-07-22 11:34:44 +0200

    remove logs

--- xmtp-anonymizer/relay.js
+++ xmtp-anonymizer/relay.js
@@ -8,21 +8,12 @@ const AI_BACKEND = process.env.AI_BACKEND;
 // XTMP message handler
 run(async (context) => {
   const message = context.message.content;
-  console.log(`RECV: ${message}`);
-
   const response = await fetch(
     `${AI_BACKEND}/?text=${encodeURIComponent(message)}`,
     {
       method: "POST",
     }
   );
-
-  // reply to XMTP message
   const text = await response.text();
-
   await context.reply(`${text}`);
-  console.log(`SENT: ${text}`);
-}).catch((err) => {
-    console.log(err);
-    }
-);
+});
