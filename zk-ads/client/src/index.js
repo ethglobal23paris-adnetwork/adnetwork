572792fe0abbde260f35dfd939167d81354e68ec
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2023-07-23 03:22:45 +0200

    worldcoin

--- zk-ads/client/src/index.js
+++ zk-ads/client/src/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 import reportWebVitals from './reportWebVitals';
 import MetamaskInfo from './helpers/MetamaskInfo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Auth0Provider
+  domain="dev-63zyub2tmag74b70.us.auth0.com"
+  clientId="UGFlphh0HK1BEcw0WAcRS55BCNJcvWP8"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+>
   <React.StrictMode>
+
     <App />
     <MetamaskInfo />
+
   </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- zk-ads/client/src/routes/Login.js
+++ zk-ads/client/src/routes/Login.js
@@ -2,31 +2,79 @@ import React, { useState, useEffect } from 'react';
 import { Button, Paper, Typography, Avatar, Box, AppBar, Toolbar } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import avatar from '../assets/MetaMask.png'
+import worldcoin from '../assets/worldcoin.png'
 import Ethereum from '../helpers/Ethereum';
+import { useAuth0 } from '@auth0/auth0-react';
 import { SpaRounded } from '@mui/icons-material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const Login = () => {
     const [state, setState] = useState([]);
     let navigate = useNavigate();
+    const {
+        isAuthenticated,
+        loginWithRedirect,
+    } = useAuth0();
 
-    async function authenticate() {
-        if (localStorage.getItem('connected') === 'true' && !(Ethereum.signer && Ethereum.provider && Ethereum.accounts.length > 0)) {
+    useEffect(() => {
+        if (Ethereum.accounts.length > 0 && isAuthenticated) 
+            navigate('/dashboard')
+    }, [isAuthenticated])
+
+    async function authenticateMetamask() {
             await Ethereum.connect();
-            if (Ethereum.accounts.length > 0) navigate('/dashboard')
+            if (Ethereum.accounts > 0 && isAuthenticated) 
+                navigate('/dashboard')
+    }
+
+    async function authenticateWorldcoin() {
+        
+        if (isAuthenticated && Ethereum.accounts > 0) {
+            navigate('/dashboard')
         }
     }
 
+    const WorldcoinLoginButton = () => {
+        return (
+            <Button variant="contained" style={{
+                color: '#0099ff', marginBottom: '10%',
+                marginRight: '15%',
+                marginLeft: '15%', marginTop: '5%',
+            }} onClick={async () => {
+                await loginWithRedirect();
+                if(isAuthenticated) {  
+                await authenticateWorldcoin(); 
+                }}}>
+                {isAuthenticated && <CheckCircleIcon color="success" style={{ marginRight: 10 }}/>}
+                <Avatar alt="Worldcoin" src={worldcoin} style={{ marginRight: 10 }} />
+                <Typography style={{ color: '#ffffff' }}>Login with Worldcoin</Typography>
+            </Button>
+        );
+    }
 
+    const MetamaskLoginButton = () => {
+        return (
+            <Button variant="contained" style={{
+                color: '#0099ff', marginBottom: '5%',
+                marginRight: '15%',
+                marginLeft: '15%', marginTop: '10%',
+            }} onClick={async () => { 
+                await authenticateMetamask() 
+            }}>
+                {Ethereum.accounts.length > 0 && <CheckCircleIcon color="success" style={{ marginRight: 10 }}/>}
+                <Avatar alt="Metamask" src={avatar} style={{ marginRight: 10 }} />
+                <Typography style={{ color: '#ffffff' }}>Login with Metamask</Typography>
+            </Button>
+        );
+    }
 
     return (
         <div>
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static" color='inherit'>
                     <Toolbar variant="dense">
-
                         {/* <img alt="Zymbit Logo" style={{ height: 50 }} src={require("")} /> */}
                     </Toolbar>
-
                 </AppBar>
             </Box>
             <Paper elevation={24} style={{
@@ -37,15 +85,8 @@ const Login = () => {
                 flexDirection: 'column',
                 alignItems: 'center',
             }}>
-
-                <Button variant="contained" style={{
-                    color: '#0099ff', marginBottom: '10%',
-                    marginRight: '15%',
-                    marginLeft: '15%', marginTop: '10%',
-                }} onClick={async () => await authenticate()}>
-                    <Avatar alt="Metamask" src={avatar} style={{ marginRight: 10 }} />
-                    <Typography style={{ color: '#ffffff' }}>Login with Metamask</Typography>
-                </Button>
+                <MetamaskLoginButton />
+                <WorldcoinLoginButton />
             </Paper>
         </div>
     );
